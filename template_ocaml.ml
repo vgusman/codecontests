ac7a8835789510b69cde33a626548362cb4e4a48
Valerii Gusman <valerii.gusman@lexifi.com>
2019-06-29 19:38:12 +0200

    More useful functions in template.

--- template_ocaml.ml
+++ template_ocaml.ml
@@ -5,37 +5,56 @@ module Big = struct
   let ( ** ) = Int64.mul
   let ( // ) = Int64.div
 
+  let babs = Int64.abs
+  let rem = Int64.rem
+
 end
 
 module Helper = struct
 
-  let read_int () = Scanf.scanf "%d\n" (fun x -> x)
-  let read_int_array n = Array.init n (fun _ -> Scanf.scanf "%d " (fun x -> x))
+  type t_type =
+    | Int of int
+    | Long of Int64.t
+
+  let read_int ?endl () =
+    match endl with
+    | None -> Scanf.scanf "%d " (fun x -> x)
+    | Some () -> Scanf.scanf "%d\n" (fun x -> x)
+
+  let read_long ?endl () =
+    match endl with
+    | None -> Scanf.scanf "%Ld " (fun x -> x)
+    | Some () -> Scanf.scanf "%Ld\n" (fun x -> x)
+
+  let read_int_array n =
+    let res = Array.init n (fun _ -> Scanf.scanf "%d " (fun x -> x)) in
+    Scanf.scanf "\n" (fun _ -> ());
+    res
+
+  let read_long_array n =
+    let res = Array.init n (fun _ -> Scanf.scanf "%Ld " (fun x -> x)) in
+    Scanf.scanf "\n" (fun _ -> ());
+    res
+
   let read_char_array n =
     let res = Array.init n (fun _ -> Scanf.scanf "%c" (fun x -> x)) in
     Scanf.scanf "\n" (fun _ -> ());
     res
 
-  type debug_type =
-    | Int of int
-    | LInt of Int64.t
-
   let debug = function
     | Int x -> Printf.printf "%d\n" x
-    | LInt x -> Printf.printf "%Ld\n" x
+    | Long x -> Printf.printf "%Ld\n" x
 
   type 'a return = { return : 'b . 'a -> 'b }
 
+  (* From Jane Street Tech Blog *)
   let with_return (type t) (f : _ -> t) =
     let module M = struct exception Return of t end in
     let return = { return = (fun x -> raise (M.Return x)); } in
     try f return with M.Return x -> x
     (* with_return (fun r -> do_somthing; if return_condition then r.return (); *)
 
-  let loop n f =
-    for i = 0 to n - 1 do
-      f i;
-    done
+  let loop n f = for i = 0 to n - 1 do f i; done
 
   let find_nb f a =
     let res = ref 0 in
@@ -46,5 +65,5 @@ end
 
 let () =
   let open Helper in
-  ()
+  (* let open Big in *)
 ;;
